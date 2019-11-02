0411c4eb89af27ff8e1f80cd6c36bd975d11f1bd
ecmnet <ecm@gmx.de>
2019-11-02 22:45:58 +0100

    variable naming

--- mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/MSPVisionPlugin.java
+++ mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/MSPVisionPlugin.java
@@ -14,33 +14,33 @@ public class MSPVisionPlugin extends MAVLinkPluginBase {
 	@Override
 	public void received(Object o) {
 
-		msg_msp_vision mocap = (msg_msp_vision) o;
-		model.vision.vx = mocap.vx;
-		model.vision.vy = mocap.vy;
-		model.vision.vz = mocap.vz;
+		msg_msp_vision vision = (msg_msp_vision) o;
+		model.vision.vx = vision.vx;
+		model.vision.vy = vision.vy;
+		model.vision.vz = vision.vz;
 
-		model.vision.cov_px = mocap.covariance[MAV_COV.VIS_COV_X];
-		model.vision.cov_py = mocap.covariance[MAV_COV.VIS_COV_Y];
-		model.vision.cov_pz = mocap.covariance[MAV_COV.VIS_COV_Z];
+		model.vision.cov_px = vision.covariance[MAV_COV.VIS_COV_X];
+		model.vision.cov_py = vision.covariance[MAV_COV.VIS_COV_Y];
+		model.vision.cov_pz = vision.covariance[MAV_COV.VIS_COV_Z];
 
-		model.vision.cov_vx = mocap.covariance[MAV_COV.VIS_COV_VX];
-		model.vision.cov_vy = mocap.covariance[MAV_COV.VIS_COV_VY];
-		model.vision.cov_vz = mocap.covariance[MAV_COV.VIS_COV_VZ];
+		model.vision.cov_vx = vision.covariance[MAV_COV.VIS_COV_VX];
+		model.vision.cov_vy = vision.covariance[MAV_COV.VIS_COV_VY];
+		model.vision.cov_vz = vision.covariance[MAV_COV.VIS_COV_VZ];
 
-		model.vision.x = mocap.x;
-		model.vision.y = mocap.y;
-		model.vision.z = mocap.z;
+		model.vision.x = vision.x;
+		model.vision.y = vision.y;
+		model.vision.z = vision.z;
 
-		model.vision.h= mocap.h;
-		model.vision.p= mocap.p;
-		model.vision.r= mocap.r;
+		model.vision.h= vision.h;
+		model.vision.p= vision.p;
+		model.vision.r= vision.r;
 
-		model.vision.qual = mocap.quality;
-		model.vision.errors = (int) mocap.errors;
+		model.vision.qual = vision.quality;
+		model.vision.errors = (int) vision.errors;
 
-		model.vision.flags = (int) mocap.flags;
-		model.vision.fps = mocap.fps;
-		if (model.vision.errors < 5 && ( mocap.vx !=0 || mocap.vy!=0)) {
+		model.vision.flags = (int) vision.flags;
+		model.vision.fps = vision.fps;
+		if (model.vision.errors < 5 && ( vision.vx !=0 || vision.vy!=0)) {
 			model.vision.tms = model.sys.getSynchronizedPX4Time_us();
 			model.sys.setSensor(Status.MSP_OPCV_AVAILABILITY, true);
 		}
