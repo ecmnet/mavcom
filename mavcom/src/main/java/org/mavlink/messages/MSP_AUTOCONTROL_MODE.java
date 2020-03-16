cfd0c7ac7479dddedd332f68f9ec1ee154b4ed23
ecmnet <ecm@gmx.de>
2020-03-16 13:06:03 +0100

    Local planner control

--- mavcom/src/main/java/org/mavlink/messages/MSP_AUTOCONTROL_MODE.java
+++ mavcom/src/main/java/org/mavlink/messages/MSP_AUTOCONTROL_MODE.java
@@ -25,6 +25,10 @@ public interface MSP_AUTOCONTROL_MODE {
      */
     public final static int COLLISION_PREVENTION = 3;
     /**
+     * PX4 local planner
+     */
+    public final static int PX4_PLANNER = 8;
+    /**
      * Interactive mode (by mouse)
      */
     public final static int INTERACTIVE = 9;
