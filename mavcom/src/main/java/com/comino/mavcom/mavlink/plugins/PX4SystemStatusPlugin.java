e95ed7a293406d0313eac2d159839d601a86df8f
ecmnet <ecm@gmx.de>
2020-06-19 09:54:43 +0200

    Battery data based on BatteryStatus message

--- mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4SystemStatusPlugin.java
+++ mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4SystemStatusPlugin.java
@@ -12,13 +12,6 @@ public class PX4SystemStatusPlugin extends MAVLinkPluginBase {
 	public void received(Object o) {
 
 		msg_sys_status sys = (msg_sys_status) o;
-		if(sys.battery_remaining != -1) {
-			model.battery.p = (short) sys.battery_remaining;
-			model.battery.b0 = sys.voltage_battery / 1000f;
-			model.battery.c0 = sys.current_battery / 100f;
-			model.battery.tms = System.currentTimeMillis() * 1000;
-		}
-
 		model.sys.error1 = sys.errors_count1;
 		if(sys.load > 0)
 			model.sys.load_p = sys.load / 10;
