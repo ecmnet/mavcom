0c213044d3694bb6eabe338d31c6315279d72fb0
ecmnet <ecm@gmx.de>
2020-06-25 09:59:59 +0200

    MavLink handling: Voltage fix

--- mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4BatteryStatusPlugin.java
+++ mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4BatteryStatusPlugin.java
@@ -11,24 +11,30 @@ public class PX4BatteryStatusPlugin extends MAVLinkPluginBase {
 	@Override
 	public void received(Object o) {
 		msg_battery_status bat = (msg_battery_status) o;
-		if (bat.current_consumed > 0)
-			model.battery.a0 = bat.current_consumed;
 
-		if(bat.battery_remaining > 0) {
-			model.battery.p = (short) bat.battery_remaining;
+		if(bat.voltages[0]<65535) {
+
+			if (bat.current_consumed > 0)
+				model.battery.a0 = bat.current_consumed;
+
+			if(bat.battery_remaining > 0) {
+				model.battery.p = (short) bat.battery_remaining;
+			}
+
+			if(bat.current_battery > 0)
+				model.battery.c0 = bat.current_battery / 100f;
+			else
+				model.battery.c0 = Float.NaN;
+
+			model.battery.b0 = 0;
+			for(int i=0;i<bat.voltages.length;i++) {
+				if(bat.voltages[i] < 65535)
+					model.battery.b0 += bat.voltages[i];
+			}
+			model.battery.b0 = model.battery.b0 / 1000f;
 			model.battery.tms = System.currentTimeMillis() * 1000;
 		}
 
-		if(bat.current_battery > 0)
-			model.battery.c0 = bat.current_battery / 100f;
-		else
-			model.battery.c0 = Float.NaN;
 
-		model.battery.b0 = 0;
-		for(int i=0;i<bat.voltages.length;i++) {
-			if(bat.voltages[i] < 65535)
-				model.battery.b0 += bat.voltages[i];
-		}
-		model.battery.b0 = model.battery.b0 / 1000f;
 	}
 }
