9270b577fb2a7cacf90f8a2ac60d6af6ed7a0e85
ecmnet <ecm@gmx.de>
2021-07-05 14:35:11 +0200

    Some utilities

---
+++ mavcom/src/main/java/com/comino/mavcom/utils/MSPUtils.java
@@ -0,0 +1,12 @@
+package com.comino.mavcom.utils;
+
+
+public class MSPUtils {
+	
+	public static boolean isChanged(int o, int n, int mask, boolean edge) {
+		if(edge)
+			return ((o & mask) != (n & mask) && ((n & mask) == mask));
+		return ((o & mask) != (n & mask) && ((n & mask) == 0));
+	}
+
+}
