e5345be82396956b666d69871fcb170bb2ba547e
ecmnet <ecm@gmx.de>
2021-07-09 12:27:28 +0200

    Parameter unified in MSPParams

---
+++ mavcom/src/main/java/com/comino/mavcom/config/MSPParams.java
@@ -0,0 +1,40 @@
+package com.comino.mavcom.config;
+
+public class MSPParams {
+	
+	// autopilot
+	
+	public static final String AUTOPILOT_FORGET_MAP     = "autopilot_forget_map";
+	public static final String AUTOPILOT_FLOW_CHECK     = "autopilot_flow_check";
+	public static final String AUTOPILOT_TAKEOFF_PROC   = "autopilot_takeoff_procedure";
+	public static final String AUTOPILOT_PRECISION_LOCK = "autopilot_precision_lock";
+	
+	
+	// control publishing
+	
+	public static final String PUBLISH_DEBUG    		= "publish_debug";
+	public static final String PUBLISH_ODOMETRY 		= "publish_odometry";
+	public static final String PUBLISH_MICROSLAM        = "publish_microslam";
+	public static final String PUBLISH_MICROGRID        = "publish_microgrid";
+	
+	
+	// T265 settings
+	
+	public static final String T265_PRECISION_LOCK      = "t265_precision_lock";
+	public static final String T265_FIDUCIAL_SIZE       = "t265_fiducial_size";
+	public static final String T265_OFFSET_X            = "t265_offset_x";
+	public static final String T265_OFFSET_Y            = "t265_offset_y";
+	public static final String T265_OFFSET_Z            = "t265_offset_z";
+	public static final String T265_CHECK_SPEED_XY      = "t265_check_speed_xy";
+	public static final String T265_CHECK_SPEED_Z       = "t265_check_speed_z";
+	
+	// D455 settings
+	
+	public static final String D455_OFFSET_X            = "d455_offset_x";
+	public static final String D455_OFFSET_Y            = "d455_offset_y";
+	public static final String D455_OFFSET_Z            = "d455_offset_z";
+	public static final String D455_WARN_OBS            = "d455_warn_obs";
+	public static final String D455_DEPTH_OVERLAY       = "d455_depth_overlay";
+	
+	
+}
