4d0272187ee9e3e88f13c9f8660b9ef82dfc5526
ecmnet <ecm@gmx.de>
2021-09-30 14:55:06 +0200

    GCL timeout increased

--- mavcom/src/main/java/com/comino/mavcom/status/StatusManager.java
+++ mavcom/src/main/java/com/comino/mavcom/status/StatusManager.java
@@ -51,7 +51,7 @@ public class StatusManager implements Runnable {
 	private static final long TIMEOUT_IMU             = 1000000;
 	private static final long TIMEOUT_VISION          = 3000000;
 	private static final long TIMEOUT_CONNECTED       = 1000000;
-	private static final long TIMEOUT_GCL_CONNECTED   = 1500000;
+	private static final long TIMEOUT_GCL_CONNECTED   = 2000000;
 	private static final long TIMEOUT_RC_ATTACHED     = 5000000;
 	private static final long TIMEOUT_JOY_ATTACHED    = 2000000;
 	private static final long TIMEOUT_GPOS            = 2000000;
