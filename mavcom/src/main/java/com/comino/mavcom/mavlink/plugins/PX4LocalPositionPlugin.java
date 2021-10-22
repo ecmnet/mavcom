144a7bab513ab96e47fa146b413b7bab7beba415
ecmnet <ecm@gmx.de>
2021-10-22 07:55:03 +0200

    Local acceleration estimate

--- mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4LocalPositionPlugin.java
+++ mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/PX4LocalPositionPlugin.java
@@ -3,9 +3,12 @@ package com.comino.mavcom.mavlink.plugins;
 import org.mavlink.messages.lquac.msg_local_position_ned;
 
 import com.comino.mavcom.model.DataModel;
+import com.comino.mavcom.model.segment.State;
 import com.comino.mavcom.model.segment.Status;
 
 public class PX4LocalPositionPlugin extends MAVLinkPluginBase {
+	
+	private static State last = new State();
 
 	public PX4LocalPositionPlugin() {
 		super(msg_local_position_ned.class);
@@ -23,11 +26,19 @@ public class PX4LocalPositionPlugin extends MAVLinkPluginBase {
 		model.state.l_vx = ned.vx;
 		model.state.l_vy = ned.vy;
 		model.state.l_vz = ned.vz;
+		model.state.tms  = DataModel.getSynchronizedPX4Time_us();
 
 		model.state.v = (float) Math.sqrt(ned.vx * ned.vx + ned.vy * ned.vy);
+		
+		if(last.tms > 0) {
+			model.state.l_ax = (model.state.l_vx - last.l_vx) * 1000f / (model.state.tms - last.tms);
+			model.state.l_ay = (model.state.l_vy - last.l_vy) * 1000f / (model.state.tms - last.tms);
+			model.state.l_az = (model.state.l_vz - last.l_vz) * 1000f / (model.state.tms - last.tms);
+		}
+		
+		last.set(model.state);
 
 		if((ned.x!=0 || ned.y!=0) && Float.isFinite(ned.x) && Float.isFinite(ned.y)) {
-			model.state.tms = DataModel.getSynchronizedPX4Time_us();
 			model.sys.setStatus(Status.MSP_LPOS_VALID, true);
 		}
 
