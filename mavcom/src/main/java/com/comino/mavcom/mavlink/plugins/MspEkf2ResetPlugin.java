1cc3a286afd7eeca114402d3d2443856e6d3a41a
ecmnet <ecm@gmx.de>
2022-08-12 10:10:09 +0200

    Plugin renamed to get registered

--- mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/MspEkf2ResetPlugin.java
+++ mavcom/src/main/java/com/comino/mavcom/mavlink/plugins/MspEkf2ResetPlugin.java
@@ -5,9 +5,9 @@ package com.comino.mavcom.mavlink.plugins;
 import org.mavlink.messages.lquac.msg_msp_ekf2_reset;
 
 
-public class MspEKF2Reset extends MAVLinkPluginBase {
+public class MspEkf2ResetPlugin extends MAVLinkPluginBase {
 
-	public MspEKF2Reset() {
+	public MspEkf2ResetPlugin() {
 		super(msg_msp_ekf2_reset.class);
 	}
 
@@ -18,6 +18,7 @@ public class MspEKF2Reset extends MAVLinkPluginBase {
 		model.state.l_rx = reset.offset_x;
 		model.state.l_ry = reset.offset_y;
 		model.state.l_rz = reset.offset_z;
+		System.out.println(o);
 	}
 
 }
