e0d394f332bc484cdd5642b7df65b8fcb8251d1a
ecmnet <ecm@gmx.de>
2022-11-08 15:10:37 +0100

    NN object detection message

--- mavcom/src/main/java/com/comino/mavcom/messaging/msgs/msp_msg_nn_object.java
+++ mavcom/src/main/java/com/comino/mavcom/messaging/msgs/msp_msg_nn_object.java
@@ -5,6 +5,6 @@ import georegression.struct.point.Point3D_F64;
 public class msp_msg_nn_object extends msp_msg_base {
 	
 	public byte          object_id     = -1;
-	public Point3D_F64   position      = new Point3D_F64();
+	public Point3D_F64   position      = new Point3D_F64(Double.NaN,Double.NaN,Double.NaN);
 
 }
